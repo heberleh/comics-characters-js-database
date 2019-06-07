648bf1ba67937d3aeecac1a32184510f89fcc106
Henry Heberle <heberleh.public@gmail.com>
2019-06-07 12:07:55 -0300

    Run unit test

--- src/MarvelDB.js
+++ src/MarvelDB.js
@@ -1,16 +1,16 @@
-const Database = require('./Database') 
+import alasql from 'alasql'
 
-const abilities = require('../dataset/comics_characters/abilities.json');
-const birthDate = require('../dataset/comics_characters/birthDate.json');
-const character = require('../dataset/comics_characters/character.json');
-const child = require('../dataset/comics_characters/child.json');
-const citizenship = require('../dataset/comics_characters/citizenship.json');
-const fictionalUniverse = require('../dataset/comics_characters/fictionalUniverse.json');
-const gender = require('../dataset/comics_characters/gender.json');
-const memberOf = require('../dataset/comics_characters/memberOf.json');
-const occupation = require('../dataset/comics_characters/occupation.json');
-const partner = require('../dataset/comics_characters/partner.json');
-const presentInWork = require('../dataset/comics_characters/presentInWork.json');
+const abilities = require('../dataset/abilities.json');
+const birthDate = require('../dataset/birthDate.json');
+const character = require('../dataset/character.json');
+const child = require('../dataset/child.json');
+const citizenship = require('../dataset/citizenship.json');
+const fictionalUniverse = require('../dataset/fictionalUniverse.json');
+const gender = require('../dataset/gender.json');
+const memberOf = require('../dataset/memberOf.json');
+const occupation = require('../dataset/occupation.json');
+const partner = require('../dataset/partner.json');
+const presentInWork = require('../dataset/presentInWork.json');
 
 function MarvelDB(){
 
@@ -20,7 +20,7 @@ function MarvelDB(){
         {name: 'character', data: character},
         {name: 'child', data: child},
         {name: 'citizenship', data: citizenship},
-        {name: 'fictionalUniverse', data: fictionalUniverse},
+        //{name: 'fictionalUniverse', data: fictionalUniverse} -->  added separatedly to use as filter
         {name: 'gender', data: gender},
         {name: 'memberOf', data: memberOf},
         {name: 'occupation', data: occupation},
@@ -28,9 +28,32 @@ function MarvelDB(){
         {name: 'presentInWork', data: presentInWork}
     ]
 
-    let db = Database(tables)
+    let db = new alasql.Database()
+
+    // Only characters registered in the Marvel Universe
+    db.exec('CREATE TABLE fictionalUniverse')
+    db.exec('INSERT INTO fictionalUniverse SELECT char FROM ? WHERE fictionalUniverseLabel LIKE "Marvel Universe"', [fictionalUniverse])
+    
+    // Create the remaining tables and add character if it is in fictionalUniverse table
+    tables.forEach(table => {  
+        // Due to ALASQL bugs, where are creating a temporary table for filtering and exporting to a temporary variable filtered_data 
+        //ALASQL bug description: I could not join the SELECT WHERE IN statement with the INSET INTO     
+        db.exec(`CREATE TABLE aux_${table.name}`);
+        db.exec(`INSERT INTO aux_${table.name} SELECT * FROM ?`, [table.data]);        
+        let filtered_data = db.exec(`SELECT * FROM aux_${table.name} WHERE aux_${table.name}.char IN (SELECT char FROM fictionalUniverse)`);
+
+        // Then, when can proceed regularly
+        db.exec(`CREATE TABLE ${table.name}`);
+        db.exec(`INSERT INTO ${table.name} SELECT * FROM ?`, [filtered_data]);
+
+        // and delete the temporary table
+        db.exec(`DROP TABLE  aux_${table.name}`);       
+    })
+
+    // Drop fictionalUniverse table -> character table contains the same elements
+    db.exec('DROP TABLE fictionalUniverse')
 
     return db
 }
-
-export default MarvelDB
+const marvelDB = MarvelDB()
+export default marvelDB
--- test/MarvelDataBase.test.js
+++ test/MarvelDataBase.test.js
@@ -1,35 +1,47 @@
-const MarvelDB = require('../src/MarvelDB')
+import marvelDB from '../src/MarvelDB'
+import { expect }  from 'chai'
+const cTable = require('console.table');
 
-describe('Products Service', function() {
-    describe('Add new product', function() {
-      //2. scenario and 3. expectation
-        it('show first 5 gender items', () => {
+describe('Marvel Database', function() {
+    describe('Database Integrity', function() {
 
-            let db = MarvelDB()
-        
-            console.log(db.exec('SELECT genderLabel FROM gender LIMIT 5'))
-        
+        beforeEach(() => this.db = marvelDB)
+           
+        it('Total number of Marvel Characters', () => {
+            let query = 'SELECT COUNT(char) as total\
+                         FROM character'
+            let result = this.db.exec(query)      
+            expect(result[0].total).to.equal(1421);        
+        });
+               
+    });
+    describe('Select Queries', function() {
+
+        beforeEach(() => this.db = marvelDB)
+           
+        it('Gender list', () => {        
+            let result = this.db.exec('SELECT DISTINCT genderLabel FROM gender');
+            //console.table(result);
+            expect(result.length).to.equal(7);
         });
         
-        it('count number of different registered abilities among genders', () => {
-        
-            let db = MarvelDB()
-        
-            console.log(db.exec('SELECT gender.genderLabel as gender, COUNT(DISTINCT abilities.abilityLabel) as abilities\
-                                FROM abilities JOIN gender ON abilities.characterId = gender.characterId\
-                                GROUP BY gender.genderLabel'))
-            
+        it('Abilities by gender', () => {        
+            let result = this.db.exec('SELECT gender.genderLabel as gender, COUNT(DISTINCT abilities.abilityLabel) as abilities\
+            FROM gender LEFT JOIN abilities ON abilities.char = gender.char\
+            GROUP BY gender.genderLabel');
+            let total = 0;
+            result.forEach(element => total += element.abilities)
+            //console.table(result);            
+            expect(total).to.equal(79);
         });
         
-        it('get details about transgender female with ability registered', () => {
-        
-            let db = MarvelDB()
-        
-            console.log(db.exec('SELECT character.characterIdLabel, gender.genderLabel, abilities.abilityLabel\
-                                    FROM character JOIN gender on character.characterId = gender.characterId\
-                                    JOIN abilities ON character.characterId = abilities.characterId\
-                                    WHERE gender.genderLabel LIKE "transgender female"'))
-            
+        it('Names of the only agender with registered abilities', () => {        
+            let result = this.db.exec('SELECT DISTINCT character.charLabel AS name\
+            FROM character JOIN gender on character.char = gender.char\
+            JOIN abilities ON character.char = abilities.char\
+            WHERE gender.genderLabel LIKE "agender"');
+            //console.table(result);   
+            expect(result[0].name).to.equal("Phoenix Force")
         });
     });
 });
